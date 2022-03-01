28270654f036c40e32e8d8c31645c22363a3e66a
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-01 20:10:44 +0100

    add html + sort in ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  public sort: string | undefined;
+  
   constructor() { }
 
   ngOnInit(): void {
